1990d3d14429c408046d5960b8240ddce0103f75
Maxime Mouial <maxime.mouial@gmail.com>
2017-06-23 00:03:06 +0200

    Adding logs command

---
+++ cli/commands/logs.py
@@ -0,0 +1,17 @@
+import click
+from cli.cli import pass_context
+from cli.helper import State
+
+
+@click.command('ps', short_help='List running recipes')
+@click.argument('recipe_id')
+@click.option('-f', '--follow', is_flag=True, help='Follow log output')
+@pass_context
+def cli(ctx, recipe_id, follow):
+    state = State.get_state(ctx)
+
+    info = state['running'].get(recipe_id)
+    if not info:
+        ctx.fail("Recipe %s isn't running" % recipe_id)
+
+    ctx.sh("%s docker-compose -f %s logs %s" % (info['options'], info['compose_file'], '-f' if follow else ''))
