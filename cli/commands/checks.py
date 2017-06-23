8f2949623e24090a27ffb1dd15a154847f077bed
eplanet <emeric.planet@datadoghq.com>
2017-06-23 14:05:34 +0200

    Add checks command

---
+++ cli/commands/checks.py
@@ -0,0 +1,12 @@
+import click
+import os
+from cli.cli import pass_context
+
+@click.command('checks', short_help='run agent checks for running containers')
+@click.argument('container_id', nargs=1, default="agent5_agent5-release_1")
+@pass_context
+def cli(ctx, container_id):
+    try:
+        ctx.sh("docker exec -it {0} /opt/datadog-agent/embedded/bin/python /opt/datadog-agent/agent/agent.py info".format(container_id))
+    except Exception as e:
+        ctx.fail("ERROR while checks: {0}".format(e))
