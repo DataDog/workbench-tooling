417fb9bd55b7283df0db61bff26dec2d918105eb
Antoine Pouille <antoine.pouille@datadoghq.com>
2017-06-23 11:12:34 +0200

    Better info message

--- cli/commands/info.py
+++ cli/commands/info.py
@@ -5,5 +5,5 @@ from cli.cli import pass_context
 @click.command('info')
 @pass_context
 def cli(ctx):
-    click.echo('This is a cli !')
+    click.echo('workbench is a small tool for easily running Datadog agent and integrations in containers with different setups')
 
