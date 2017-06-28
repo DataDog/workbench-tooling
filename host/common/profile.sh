a746f8ffd2a1218a39860a61faf6964e44fd45f6
Xavier Vello <xavier.vello@datadoghq.com>
2017-06-28 12:01:58 +0200

    improve ubuntu vagrant
    - cleanup provisioning scripts
    - install workbench bash completion and wb alias

--- host/common/profile.sh
+++ host/common/profile.sh
@@ -3,3 +3,8 @@
 # It can be used to add common envvars and aliases
 
 export PATH=/opt/workbench/host/common/bin/:$PATH
+
+if hash workbench 2>/dev/null; then
+    eval "$(_WORKBENCH_COMPLETE=source workbench)"
+    alias wb=workbench
+fi
